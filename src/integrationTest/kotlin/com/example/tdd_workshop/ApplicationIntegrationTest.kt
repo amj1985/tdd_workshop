c33072bb2220efc0a15114daa9d9759fecdc5a40
Adrià Manzano <adria.manzano@adevinta.com>
2022-02-20 12:28:42 +0100

    finish scaffolding

--- src/integrationTest/kotlin/com/example/tdd_workshop/ApplicationIntegrationTest.kt
+++ src/integrationTest/kotlin/com/example/tdd_workshop/ApplicationIntegrationTest.kt
@@ -1,19 +1,30 @@
 package com.example.tdd_workshop
 
 import com.example.tdd_workshop.helper.DockerComposeHelper
+import io.restassured.module.mockmvc.RestAssuredMockMvc
 import org.junit.Assert
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Testcontainers
 @SpringBootTest(
     classes = [Application::class],
-    properties = ["spring.profiles.active=integration-test"]
+    properties = ["spring.profiles.active=integration-test"],
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
+@AutoConfigureMockMvc
 class ApplicationIntegrationTest {
+
+    @Autowired
+    lateinit var mvc: MockMvc
 
     companion object {
 
@@ -25,6 +36,11 @@ class ApplicationIntegrationTest {
         fun setSystemProperties() {
             DockerComposeHelper.setSystemProperties(dockerComposeContainer)
         }
+    }
+
+    @BeforeEach
+    fun setUp() {
+        RestAssuredMockMvc.mockMvc(mvc)
     }
 
 }
